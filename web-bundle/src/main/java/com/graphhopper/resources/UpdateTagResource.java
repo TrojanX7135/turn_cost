c266dbd1258ca5117655703b285153c7c8ac57b2
Tran Thanh Tung <ttung6618@gmail.com>
2023-12-11 09:09:38 +0700

    commit for last week

--- web-bundle/src/main/java/com/graphhopper/resources/UpdateTagResource.java
+++ web-bundle/src/main/java/com/graphhopper/resources/UpdateTagResource.java
@@ -5,8 +5,10 @@ import com.graphhopper.reader.ReaderWay;
 import com.graphhopper.storage.IntsRef;
 
 import javax.inject.Inject;
+import javax.validation.constraints.NotNull;
 import javax.ws.rs.*;
 import javax.ws.rs.core.*;
+import java.util.ArrayList;
 import java.util.List;
 
 import static java.lang.Long.parseLong;
@@ -24,25 +26,36 @@ public class UpdateTagResource {
     @GET
     @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, "application/gpx+xml"})
     public String doGet(
-            @QueryParam("OSMId") String OSMId,
-            @QueryParam("key") String key,
-            @QueryParam("value") String value)
+            @QueryParam("OSMId") @DefaultValue("0")String OSMId,
+            @QueryParam("key") @DefaultValue("None") String key,
+            @QueryParam("value") @DefaultValue("None") String value)
     {
         ReaderWay way= this.graphHopper.getReader().getWaysegment().getWayFromOSMId(parseLong(OSMId));
         if(way != null)
         {
-            System.out.print("Old value: ");
-            System.out.println(way.getTag(key));
-            way.setTag(key,value);
             List<Integer> listEdge = this.graphHopper.getReader().getEgdeFromWay(parseLong(OSMId));
-            IntsRef relationFlags = this.graphHopper.getReader().getRelFlagsMap(parseLong(OSMId));
-            for (Integer integer : listEdge)
+            if(key.contains("None") && value.contains("None"))
             {
-                this.graphHopper.getOSMParsers().handleWayTags(integer, this.graphHopper.getBaseGraph().createEdgeIntAccess(), way, relationFlags);
+                List<String> keysList = new ArrayList<>(way.getTags().keySet());
+                for (String s : keysList) {
+                    if(s.contains("key_values")) break;
+                    System.out.println(s);
+                }
             }
-            System.out.print("New value: ");
-            System.out.println(way.getTag(key));
-            return "OKE";
+            else
+            {
+                System.out.print("Old value: ");
+                System.out.println(way.getTag(key));
+                way.setTag(key,value);
+                IntsRef relationFlags = this.graphHopper.getReader().getRelFlagsMap(parseLong(OSMId));
+                for (Integer integer : listEdge)
+                {
+                    this.graphHopper.getOSMParsers().handleWayTags(integer, this.graphHopper.getBaseGraph().createEdgeIntAccess(), way, relationFlags);
+                }
+                System.out.print("New value: ");
+                System.out.println(way.getTag(key));
+                return "OKE";
+            }
         }
         return "FAIL";
     }
